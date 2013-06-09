2227df3f9af4a74303c914c56af80d76348589c9
Chris Saylor <christopher.saylor@zumba.com>
2013-06-09 08:58:18 -0400

    Moved socket server listener to master process.
    Work is still deligated to the workers via the master, but the master
    no longer is relying on a worker to send it work to be distributed.

--- src/server.js
+++ src/server.js
@@ -7,61 +7,14 @@ var cluster = require('cluster'),
 	async = require('async');
 
 // Config the UNIX socket file
-var SOCK_FILE = process.env.SOCK_FILE || '/tmp/zlog.sock';
-var WORKERS = process.env.WORKERS || require('os').cpus().length;
-var chunkDelimiter = ';';
+var SOCK_FILE = process.env.SOCK_FILE || '/tmp/zlog.sock',
+	WORKERS = process.env.WORKERS || require('os').cpus().length,
+	chunkDelimiter = process.env.DELIMITER || ';;',
+	worker;
 
-if (cluster.isMaster) {
-	
-	fs.unlink(SOCK_FILE, function (error) {
-		if (error) {
-			console.log('Error removing old sock file', error);
-			return;
-		}
-	});
-
-	console.log('Master process starting.');
-	for (var i = WORKERS; i > 0; i--) {
-		console.log('Starting worker');
-		cluster.fork();
-	}
-
-	cluster.on('disconnect', function (worker) {
-		console.log('Worker disconnected!');
-	});
-
-	cluster.on('exit', function (worker, code, signal) {
-		console.error('Worker ' + worker.process.pid + ' died');
-		if (worker.suicide) {
-			cluster.fork();
-		}
-	});
-
-	// Be a good master and listen to your workers!
-	Object.keys(cluster.workers).forEach(function(id) {
-		cluster.workers[id].on('message', function(workerMessage) {
-			var split, workToBeDone = [];
-			// Worker told me I have work to delegate
-			if (workerMessage.type === 'received') {
-				workToBeDone = workerMessage.data.split(chunkDelimiter);
-				split = Math.ceil(workToBeDone.length / WORKERS);
-				for (var i in cluster.workers) {
-					cluster.workers[i].send({
-						type: 'work',
-						data: workToBeDone.slice(0, split)
-					});
-					workToBeDone.splice(0, split);
-				}
-			}
-		});
-	});
-
-} else {
-
-	// @todo implement json require to get winston transports to instantiate
-	winston.add(winston.transports.File, { filename: '/tmp/out.log' });
-	winston.remove(winston.transports.Console);
-	var logChunkContent = function(chunk) {
+// Worker API
+var workerApi = {
+	logChunkContent: function(chunk) {
 		var params, content;
 		try {
 			chunk = chunk.trim();
@@ -86,31 +39,65 @@ if (cluster.isMaster) {
 			params
 		);
 	}
+}
+
+if (cluster.isMaster) {
+
+	// Remove old socket files
+	fs.unlink(SOCK_FILE, function (error) {
+		if (error) {
+			console.log('Error removing old sock file', error);
+			return;
+		}
+	});
+
+	console.log('Master process starting.');
+	for (var i = WORKERS; i > 0; i--) {
+		worker = cluster.fork();
+		console.log('Worker[' + worker.id + '] started.');
+	}
+
+	cluster.on('disconnect', function (worker) {
+		console.log('Worker disconnected!');
+	});
+
+	cluster.on('exit', function (worker, code, signal) {
+		console.error('Worker ' + worker.process.pid + ' died');
+		if (worker.suicide) {
+			cluster.fork();
+		}
+	});
+
+	// Be a good master and listen to your workers!
+	Object.keys(cluster.workers).forEach(function(id) {
+		cluster.workers[id].on('message', function(workerMessage) {
+			// Placeholder for possible messages from this worker
+		});
+	});
 
 	// Create the socket listener
 	var server = net.createServer(function (socket) {
-		var socketData = '';
+		var socketData = '', workToBeDone, split;
 		socket.setEncoding('utf8');
+
 		// Compile chunks for this request
 		socket.on('data', function (data) {
 			socketData += data;
 		});
-		// Pass the compiled chunk up to the master to delegate
+
+		// Delegate the work to the workers
 		socket.on('end', function() {
-			process.send({
-				type: 'received',
-				data: socketData
-			});
+			workToBeDone = socketData.split(chunkDelimiter);
+			split = Math.ceil(workToBeDone.length / WORKERS);
+			for (var i in cluster.workers) {
+				cluster.workers[i].send({
+					type: 'work',
+					data: workToBeDone.slice(0, split)
+				});
+				workToBeDone.splice(0, split);
+			}
 			socketData = '';
 		});
-	});
-
-	// Listen to messages from the master
-	process.on('message', function(message) {
-		// Master told me I have work to do!
-		if (message.type === 'work') {
-			async.each(message.data, logChunkContent);
-		}
 	});
 
 	// Start the server
@@ -120,6 +107,26 @@ if (cluster.isMaster) {
 				console.log('Socket permission failed.', error);
 			}
 		});
-		console.log('Worker listening to socket.');
+		console.log('Master listening to socketfile: ' + SOCK_FILE);
 	});
+
+} else {
+
+	// Setup winston logging
+	// @todo implement json require to get winston transports to instantiate
+	winston.add(winston.transports.File, { filename: '/tmp/out.log' });
+	winston.remove(winston.transports.Console);
+
+	// Listen to messages from the master
+	process.on('message', function(message) {
+		// Master told me I have work to do!
+		switch (message.type) {
+			case 'work':
+				async.each(message.data, workerApi.logChunkContent);
+				break;
+			default:
+				console.log('Worker does not understand message.');
+		}
+	});
+
 }
